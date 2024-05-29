e68ee57f4686d07ab0d4c9abac62e960c4181b94
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-05-29 10:08:43 +0200

    update comment modal en dashuser

--- Dashboard/src/Components/DashUsers/DashUsers.jsx
+++ Dashboard/src/Components/DashUsers/DashUsers.jsx
@@ -144,7 +144,7 @@ export default function DashUsers() {
                   </div>
                   <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
                     <h3 className='text-lg leading-6 font-medium text-gray-900' id='modal-title'>
-                      Are you sure you want to delete this user?
+                      ¿Quieres borrar este usuario?
                     </h3>
                   </div>
                 </div>
@@ -154,13 +154,13 @@ export default function DashUsers() {
                   onClick={handleDeleteUser}
                   className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm'
                 >
-                  Yes, I'm sure
+                  Sí, estoy seguro
                 </button>
                 <button
                   onClick={() => setShowModal(false)}
                   className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
                 >
-                  No, cancel
+                  No, cancela
                 </button>
               </div>
             </div>
